74c6fc9d768afd8327a3bab310fbee56581f68ae
koretskiyav <koretskiyav@gmail.com>
2021-02-13 19:53:03 +0200

    add header, normalize.css, styles, move to folders

--- src/hocs/counter.js
+++ src/hocs/counter.js
@@ -6,20 +6,3 @@ export default (WrappedComponent) => (props) => {
   const amountProps = useAmount(0);
   return <WrappedComponent {...props} {...amountProps} />;
 };
-
-// export default (WrappedComponent) => {
-//   const HocComponent = (props) => {
-//     const { decrement, increment, amount } = useAmount(0);
-
-//     return (
-//       <WrappedComponent
-//         {...props}
-//         decrement={decrement}
-//         increment={increment}
-//         amount={amount}
-//       />
-//     );
-//   };
-
-//   return HocComponent;
-// };
