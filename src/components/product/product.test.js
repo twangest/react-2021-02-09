fc52e66c2d0c01aa04d75805d35148f70aff2974
Alexey Struchenkov <forcorp@inbox.ru>
2021-02-17 15:07:27 +0200

    ht2.2 add product-decrement test

--- src/components/product/product.test.js
+++ src/components/product/product.test.js
@@ -22,6 +22,11 @@ describe('Product', () => {
     wrapper.find('[data-id="product-increment"]').simulate('click');
     expect(wrapper.find('[data-id="product-amount"]').text()).toBe('1');
   });
+  it('should decrement amount', () => {
+    const wrapper = mount(<Product product={product} amount={1} />);
+    wrapper.find('[data-id="product-decrement"]').simulate('click');
+    expect(wrapper.find('[data-id="product-amount"]').text()).toBe('0');
+  });
   it('should fetch data', () => {
     const fn = jest.fn();
     mount(<Product product={product} fetchData={fn} />);
