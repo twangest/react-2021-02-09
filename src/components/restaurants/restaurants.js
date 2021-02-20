510393b0288609532ec1835ac9f13ce24a9da162
koretskiyav <koretskiyav@gmail.com>
2021-02-20 13:49:01 +0200

    add Tabs component

--- src/components/restaurants/restaurants.js
+++ src/components/restaurants/restaurants.js
@@ -1,25 +1,14 @@
-import React, { useState, useMemo } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Restaurant from '../restaurant';
-import Navigation from '../navigation';
+import Tabs from '../tabs';
 
 const Restaurants = ({ restaurants }) => {
-  const [activeRestaurantId, setActiveRestaurant] = useState(restaurants[0].id);
-
-  const activeRestaurant = useMemo(
-    () => restaurants.find(({ id }) => id === activeRestaurantId),
-    [activeRestaurantId, restaurants]
-  );
-
-  return (
-    <div>
-      <Navigation
-        restaurants={restaurants}
-        onRestaurantClick={setActiveRestaurant}
-      />
-      <Restaurant restaurant={activeRestaurant} />
-    </div>
-  );
+  const tabs = restaurants.map((restaurant) => ({
+    title: restaurant.name,
+    content: <Restaurant restaurant={restaurant} />,
+  }));
+  return <Tabs tabs={tabs} />;
 };
 
 Restaurants.propTypes = {
