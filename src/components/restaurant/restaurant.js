1406d23c8c7041d9a1d28add9ce6b0ff88a6cb98
koretskiyav <koretskiyav@gmail.com>
2021-03-05 20:00:11 +0200

    add Redirect

--- src/components/restaurant/restaurant.js
+++ src/components/restaurant/restaurant.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Route } from 'react-router-dom';
+import { Route, Redirect, Switch } from 'react-router-dom';
 import { createStructuredSelector } from 'reselect';
 import PropTypes from 'prop-types';
 import Menu from '../menu';
@@ -23,12 +23,19 @@ const Restaurant = ({ restaurant, averageRating }) => {
         {!!averageRating && <Rate value={averageRating} />}
       </Banner>
       <Tabs tabs={tabs} />
-      <Route path="/restaurants/:restId/menu">
-        <Menu menu={menu} restaurantId={id} />
-      </Route>
-      <Route path="/restaurants/:restId/reviews">
-        <Reviews reviews={reviews} restaurantId={id} />
-      </Route>
+      <Switch>
+        <Route path="/restaurants/:restId/menu">
+          <Menu menu={menu} restaurantId={id} />
+        </Route>
+        <Route path="/restaurants/:restId/reviews">
+          <Reviews reviews={reviews} restaurantId={id} />
+        </Route>
+        <Redirect
+          // exact
+          // from="/restaurants/:restId"
+          to={`/restaurants/${id}/menu`}
+        />
+      </Switch>
     </div>
   );
 };
